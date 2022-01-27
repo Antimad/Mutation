f087c971bb68f3d20e6e1ab3cf5603f7a5812f92
Antimad <udnwaege@gmail.com>
2022-01-26 20:40:47 -0800

    testing

--- Inference.py
+++ Inference.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 file = r"C:\Users\unwae\PycharmProjects\UCR_W21-Wright_Fisher_Model\Sample.json"
 
@@ -8,14 +10,11 @@ sample = pd.read_json(file)
 sample_properties = {
     "pop_size": 1000,
     "S_Length": 50,
-    "Generations": 100,
+    "Generations": 1000,
     "Mutation Rate": 0.001,
     "Beneficial_Deleterious Mutation": 10,
     "Beneficial_Deleterious Fitness": 0.03
-
 }
-
-sequence_size_matrix = sample[0].to_numpy()
 
 
 def calc_xij(generation, i, j):
@@ -54,43 +53,57 @@ def covariance_builder(generation: np.array, size: np.array):
     return covariance_matrix
 
 
-seq = np.array(sequence_size_matrix[0][2])
-Size = np.array(sequence_size_matrix[1][2])
-Covariance = covariance_builder(generation=seq, size=Size)
-
-mu = 0
-for time, sequences in enumerate(sequence_size_matrix[0]):
-    """
-    The main loop handles time, as each sequence is a point(generation) in time. 
-    einsum is used to handle the summations of indices i & j.
-    """
-    """
-    The equation has indices i & j, for sites i & j.
-    Since I'll be using einsum for the summations of i & j, I will reference 
-    """
-    Covariance = covariance_builder(generation=np.array(sequences), size=np.array(sequence_size_matrix[1][time]))
-    Sum_C_ik = np.einsum('ik->k', Covariance)
-    Sum_C_ik_S = Sum_C_ik * sample_properties["Beneficial_Deleterious Fitness"]
-    Covariance[np.diag_indices_from(Covariance)] += 1  # Regularization
-    inverse_Covariance = np.linalg.inv(Covariance)
-    top = bottom = 0
-    for idx, sequence in enumerate(sequences):
+def main(gen_data):
+    mu = 0
+    regularization = 1 / (sample_properties["pop_size"])
+    for time, sequences in enumerate(gen_data["Sequence"]):
         """
-        This loop handle the iteration of each sequence in the generation (time) loop above.
+        The main loop handles time, as each sequence is a point(generation) in time. 
+        einsum is used to handle the summations of indices i & j.
         """
-        delta_X_i = sequence[-1] - sequence[0]  # Should this be the other way around?
-        delta_xi_minus_sumCs = delta_X_i - Sum_C_ik_S
+        """
+        The equation has indices i & j, for sites i & j.
+        Since I'll be using einsum for the summations of i & j, I will reference 
+        """
+        covariance = covariance_builder(generation=np.array(sequences), size=np.array(gen_data["Size"][time]))
+        sum_c_ik = np.einsum('ik->k', covariance)
+        sum_c_ik_s = sum_c_ik * sample_properties["Beneficial_Deleterious Fitness"]
+        covariance[np.diag_indices_from(covariance)] += regularization  # Regularization
+        inverse_covariance = np.linalg.inv(covariance)
+        top = bottom = 0
+        for idx, sequence in enumerate(sequences):
+            """
+            This loop handle the iteration of each sequence in the generation (time) loop above.
+            """
+            delta_x_i = sequence[0] - sequence[-1]  # Should this be the other way around?
+            delta_xi_minus_sum_cs = delta_x_i - sum_c_ik_s
 
-        for index_i, site_i in enumerate(sequence):
-            one_minus_2xi = (1 - (2 * site_i))
+            for index_i, site_i in enumerate(sequence):
+                one_minus_2xi = (1 - (2 * site_i))
 
-            for index_j, site_j in enumerate(sequence):
-                one_minus_2xj = (1 - (2 * site_j))
-                invC_times_1_2_x_j = (inverse_Covariance[index_i][index_j] * one_minus_2xj)
-                top += (delta_xi_minus_sumCs[index_i] * invC_times_1_2_x_j)
-                bottom += (one_minus_2xi * invC_times_1_2_x_j)
+                for index_j, site_j in enumerate(sequence):
+                    one_minus_2xj = (1 - (2 * site_j))
+                    inv_c_times_1_2_x_j = (inverse_covariance[index_i][index_j] * one_minus_2xj)
+                    top += (delta_xi_minus_sum_cs[index_i] * inv_c_times_1_2_x_j)
+                    bottom += (one_minus_2xi * inv_c_times_1_2_x_j)
+        mu = top/bottom
+    return mu
 
-    if bottom == 0:
-        print("Zero at time={0}}".format(time))
-        continue
-    mu += (top/bottom)
+
+results = []
+index = 0
+
+for root, dirs, files in os.walk("Data", topdown=False):
+    for file in files:
+        index += 1
+        data = np.load(os.path.join(root, file), allow_pickle=True)
+        result = main(gen_data=data)
+        results.append(result)
+        print("Result: {0}, index: {0}".format(result, index))
+
+
+plt.hist(x=results)
+plt.ylabel("Mutation Rates")
+plt.title("Mutation Rate Distribution")
+
+plt.savefig("Mutation Distribution")
